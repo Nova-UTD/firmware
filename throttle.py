35411947abfb30c03499c0feb7dd64e39291556a
Jai Peris <93809301+jaiperis@users.noreply.github.com>
2023-02-08 18:30:56 -0600

    Update throttle.py

--- throttle.py
+++ throttle.py
@@ -1,8 +1,5 @@
-# Throttle By Wire in Circuit Python
-# download the CircuitPython Analog Out libraries
+# Throttle By Wire in Circuit Python 
 # ex. 5000 / 64 = 78, and 78 / 1024 * 3.3V = 0.25V output.
-# 1.55V = 30782
-# 2.0V = 39718
 
 import board
 from analogio import AnalogOut
@@ -11,16 +8,18 @@ from analogio import AnalogOut
 def calc_volt(volt):
     return ((volt / 3.3) * 1024) * 64
 
-def speed(x):
-    volt_o = (0.0095*x) + 0.7745
-    volt_b = (0.0168*x) + 1.4956
-    return [volt_o, volt_b]
+def voltage(x):
+    volt_o = (0.1809*x) + 0.8335
+    volt_b = (0.3949*x) + 1.6996
+    return volt_o, volt_b
 
 analog_outO = AnalogOut(board.A0)
 analog_outB = AnalogOut(board.A1)
 
 while (True):
-    input_speed = input('Enter speed: ')
-    spd = speed(input_speed)
-    analog_outO.value = int(calc_volt(spd[0]))
-    analog_outB.value = int(calc_volt(spd[1]))
+    input_num = float(input('Enter num between 0 and 1: '))
+    volt = voltage(input_num)
+    analog_outO.value = int(calc_volt(volt[0]))
+    analog_outB.value = int(calc_volt(volt[1]))
+    print(volt[0])
+    print(volt[1])
